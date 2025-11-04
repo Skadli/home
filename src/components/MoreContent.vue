673fbe4ada6dfead7ab44685c7a9ebf3f3a7666c
Skadli <1350375634@qq.com>
2025-11-04 09:12:24 +0800

    update

---
+++ src/components/MoreContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="more-content">看到此页面可以联系客服免费体验任意一个服务三天！</div>
+</template>
+
+<style lang="scss" scoped>
+.more-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,88 @@
+/* eslint-disable no-undef */
+import { defineConfig, loadEnv } from "vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import { resolve } from "path";
+import { VitePWA } from "vite-plugin-pwa";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import viteCompression from "vite-plugin-compression";
+
+// https://vitejs.dev/config/
+export default ({ mode }) =>
+  defineConfig({
+    plugins: [
+      vue(),
+      AutoImport({
+        imports: ["vue"],
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      VitePWA({
+        registerType: "autoUpdate",
+        workbox: {
+          skipWaiting: true,
+          clientsClaim: true,
+          runtimeCaching: [
+            {
+              urlPattern: /(.*?)\.(js|css|woff2|woff|ttf)/, // js / css 静态资源缓存
+              handler: "CacheFirst",
+              options: {
+                cacheName: "js-css-cache",
+              },
+            },
+            {
+              urlPattern: /(.*?)\.(png|jpe?g|svg|gif|bmp|psd|tiff|tga|eps)/, // 图片缓存
+              handler: "CacheFirst",
+              options: {
+                cacheName: "image-cache",
+              },
+            },
+          ],
+        },
+        manifest: {
+          name: loadEnv(mode, process.cwd()).VITE_SITE_NAME,
+          short_name: loadEnv(mode, process.cwd()).VITE_SITE_NAME,
+          description: loadEnv(mode, process.cwd()).VITE_SITE_DES,
+          display: "standalone",
+          start_url: "/",
+          theme_color: "#424242",
+          background_color: "#424242",
+          icons: [
+            
+          ],
+        },
+      }),
+      viteCompression(),
+    ],
+    server: {
+      port: "3300",
+      open: true,
+    },
+    resolve: {
+      alias: [
+        {
+          find: "@",
+          replacement: resolve(__dirname, "src"),
+        },
+      ],
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          charset: false,
+          additionalData: `@import "./src/style/global.scss";`,
+        },
+      },
+    },
+    build: {
+      minify: "terser",
+      terserOptions: {
+        compress: {
+          pure_funcs: ["console.log"],
+        },
+      },
+    },
+  });
